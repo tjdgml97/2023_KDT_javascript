18f0f663efbb8b68c86898054ad2b3b22faa6fd5
tjdgml97 <parksunghee9@naver.com>
2023-02-19 23:35:09 +0900

    todolist add input placeholder

--- Todolist/todo.js
+++ Todolist/todo.js
@@ -1,61 +1,67 @@
-// 클릭이 발생했을때 함수 실행 , event,함수 
-const addbtn = document.querySelector('');
+const inputTask = document.querySelector(".input-task");
 
-function deleteTask(t) {
-  t.parentNode.remove();
+function addTodo() {
+  if(inputTask==="") inputTask.setAttribute("placeholder","내용을 입력하세요");
 }
 
-function addList(){
-  // input tas - input 에 값이 아무것도 없으면 - placeholder(속성)변경 
-  // return - > if else 대신 if 문에서 return 으로 종료 , 
-  if(inputTask.value==="") {
-    inputTask.setAttribute("placeholder","내용을 입력하세요");
-    return;
-  }
+// // 클릭이 발생했을때 함수 실행 , event,함수 
+// const addbtn = document.querySelector('');
 
-  const addLi = document.createElement("li")
-  // input 인데 속성이 check
-  const checkBtn = document.createElement("input");
-  checkBtn.setAttribute("type","checkbox");
-  // event 가 발생할떄 - 어떤이벤트, 함수 
-  checkBtn.addEventListener("click",function(){
-    if(checkBtn.checked===true){
-      checkBtn.parentNode.style.textDecoration = "line-through";
-    } else{
-      checkBtn.parentNode.setAttribute("style","text-decoration: none")
-    }
-  })
+// function deleteTask(t) {
+//   t.parentNode.remove();
+// }
 
+// function addList(){
+//   // input tas - input 에 값이 아무것도 없으면 - placeholder(속성)변경 
+//   // return - > if else 대신 if 문에서 return 으로 종료 , 
+//   if(inputTask.value==="") {
+//     inputTask.setAttribute("placeholder","내용을 입력하세요");
+//     return;
+//   }
 
-  const deleteBtn = document.createElement("button");
-  deleteBtn.textContent = "삭제";
-
-  // 방법2 
-  // 속성을 설정 온클릭 -> 디스를 전달
-  //Q1
-  deleteBtn.setAttribute("onclick","deleteTask(this)")
-
-  // 방법 1
-  // deleteBtn.addEventListener("click",function(){
-    // event target 사용 - 어떤 delete버튼이 사용되었는지 확인
-    // 부모 요소인 li 지워짐 
-    // e.target.parentNode.remove();
-  // })
-  // append child 는 붙지 않음 
-  addLi.append(checkBtn)
-  addLi.append(inputTask.value) //글자 컨텐츠 붙임 
-  addLi.append(deleteBtn)
+//   const addLi = document.createElement("li")
+//   // input 인데 속성이 check
+//   const checkBtn = document.createElement("input");
+//   checkBtn.setAttribute("type","checkbox");
+//   // event 가 발생할떄 - 어떤이벤트, 함수 
+//   checkBtn.addEventListener("click",function(){
+//     if(checkBtn.checked===true){
+//       checkBtn.parentNode.style.textDecoration = "line-through";
+//     } else{
+//       checkBtn.parentNode.setAttribute("style","text-decoration: none")
+//     }
+//   })
 
 
-  todoList.appendChild(addLi);
+//   const deleteBtn = document.createElement("button");
+//   deleteBtn.textContent = "삭제";
+
+//   // 방법2 
+//   // 속성을 설정 온클릭 -> 디스를 전달
+//   //Q1
+//   deleteBtn.setAttribute("onclick","deleteTask(this)")
+
+//   // 방법 1
+//   // deleteBtn.addEventListener("click",function(){
+//     // event target 사용 - 어떤 delete버튼이 사용되었는지 확인
+//     // 부모 요소인 li 지워짐 
+//     // e.target.parentNode.remove();
+//   // })
+//   // append child 는 붙지 않음 
+//   addLi.append(checkBtn)
+//   addLi.append(inputTask.value) //글자 컨텐츠 붙임 
+//   addLi.append(deleteBtn)
 
 
-  // Q2
-  inputTask.value="";  
-  addbtn.addEventListener("click",addList);
-  addbtn.addEventListener("click",addList);
+//   todoList.appendChild(addLi);
 
-  // 
-}
 
-addbtn.addEventListener("click",addList);
+//   // Q2
+//   inputTask.value="";  
+//   addbtn.addEventListener("click",addList);
+//   addbtn.addEventListener("click",addList);
+
+//   // 
+// }
+
+// addbtn.addEventListener("click",addList);
